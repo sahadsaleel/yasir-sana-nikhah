7b80f8870acf290cd1197c7969bc645597698d01
Sahad Saleel <saleelsahad@gmail.com>
2026-06-08 18:31:10 +0530

    add a new loading system

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'))
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+// Hide splash once React has painted
+requestAnimationFrame(() => {
+  requestAnimationFrame(() => {
+    const splash = document.getElementById('splash-screen')
+    if (splash) splash.classList.add('hidden')
+  })
+})
